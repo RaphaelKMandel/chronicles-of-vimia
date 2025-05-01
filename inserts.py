573924045a4f461c839c8c476a3b1e3b5ca1f984
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-05-01 16:41:07 -0400

    add InsertAtStart and AppendAtEnd

--- inserts.py
+++ inserts.py
@@ -1,11 +1,8 @@
 from constants import *
+from horizontal_movements import Carrot, Dollar
 
 
-
-
-
-
-class Insert:
+class Insert(Action):
     def __init__(self, parent):
         self.parent = parent
         InsertMode(parent)
@@ -18,5 +15,21 @@ class Append:
         InsertMode(parent)
 
 
+class InsertAtStart(Action):
+    def __init__(self, parent):
+        self.parent = parent
+        EDITOR.buffer.col = Carrot(parent).evaluate(EDITOR.buffer.lines, EDITOR.buffer.row, EDITOR.buffer.col)
+        Insert(parent)
+
+
+class AppendAtEnd(Action):
+    def __init__(self, parent):
+        self.parent = parent
+        EDITOR.buffer.col = Dollar(parent).evaluate(EDITOR.buffer.lines, EDITOR.buffer.row, EDITOR.buffer.col)
+        Append(parent)
+
+
 NormalMode.KEYMAP["i"] = Insert
 NormalMode.KEYMAP["a"] = Append
+NormalMode.KEYMAP["I"] = InsertAtStart
+NormalMode.KEYMAP["A"] = AppendAtEnd
