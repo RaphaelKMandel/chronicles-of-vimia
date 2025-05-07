d3eda7ed8721d6a0425cb34c51fde8bdf4067e86
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-05-06 20:41:29 -0400

    basic word movements using a word_parser.py

--- word_parser.py
+++ word_parser.py
@@ -1,5 +1,5 @@
 class WordParser:
-    SPECIAL = "{}[]():,.'\"-+/"
+    SPECIAL = "{}[]():,.'\"-+=/"
 
     def __str__(self):
         return str([self.words, self.ind])
@@ -7,18 +7,16 @@ class WordParser:
     def __init__(self, text):
         self.text = text
         self.state = None
-        self.words = self.get_words()
-        self.ind = self.get_ind()
+        self.words = self._get_words()
+        self.ind = self._get_ind()
+        self.word_inds = self._get_word_ind()
 
-    def get_words(self):
+    def _get_words(self):
         word = ""
         words = []
         for char in self.text:
-            state = self.get_state(char)
-            if (
-                    (self.state == "space" and state != "space")
-                    or (self.state == "printable" and state == "special")
-                    or (self.state == "special" and state == "printable")):
+            state = self._get_state(char)
+            if state != self.state:
                 words += [word]
                 word = ""
 
@@ -27,9 +25,12 @@ class WordParser:
 
         words += [word]  # Clean up any remaining word
 
+        if len(words) > 1:
+            words = words[1:]  # Remove first word which is an empty string
+
         return words
 
-    def get_ind(self):
+    def _get_ind(self):
         n = 0
         ind = []
         for word in self.words:
@@ -38,7 +39,17 @@ class WordParser:
 
         return ind
 
-    def get_state(self, char):
+    def _get_word_ind(self):
+        inds = []
+        i = 0
+        for n, word in enumerate(self.words):
+            ind = list(range(i, i + len(word)))
+            inds += [ind]
+            i += len(word)
+
+        return inds
+
+    def _get_state(self, char):
         if char == " ":
             return "space"
 
@@ -48,11 +59,32 @@ class WordParser:
         if char.isprintable():
             return "printable"
 
+    def next_word_start(self, ind):
+        for word, inds in zip(self.words, self.word_inds):
+            if inds[0] > ind and word.strip():
+                return inds[0]
+
+    def next_word_end(self, ind):
+        for word, inds in zip(self.words, self.word_inds):
+            if inds[-1] > ind and word.strip():
+                return inds[-1]
+
+    def prev_word_start(self, ind):
+        for word, inds in zip(reversed(self.words), reversed(self.word_inds)):
+            print(word, inds)
+            if inds[0] < ind and word.strip():
+                return inds[0]
+
 
 if __name__ == "__main__":
-    wp = WordParser("This is (a) test phrase.")
+    wp = WordParser("def foo(x, y=[1,2,3]):")
+    wp = WordParser("class Foo:    def foo(self, x):      return self.x + 1")
     print(wp.words)
+    print(wp.word_inds)
     print(wp.ind)
 
-    wp = WordParser("")
-    print(wp)
+    # for n, char in enumerate(wp.text):
+    #     print("main", n, char, wp.next_word_start(n), wp.next_word_end(n))
+
+    for n, char in enumerate(wp.text):
+        print("main", n, char, wp.prev_word_start(n))
--- words.py
+++ words.py
@@ -1,57 +1,47 @@
 from constants import *
-
-WORD_BOUNDARIES = "{}[](),.'\"-+:/"
-
-# Test {}[](),.'"
+from word_parser import WordParser
 
 
-class WordMovement(InstantMovement):
+class WordMovements(InstantMovement):
     def execute(self):
         col = self.evaluate(EDITOR.buffer)
-        if col:
+        if col is not None:
             EDITOR.buffer.col = col
 
+
+class StartWord(WordMovements):
     def evaluate(self, buffer):
-        return self.find_next_word(buffer.line, buffer.col)
+        col = WordParser(buffer.line).next_word_start(buffer.col)
+        if col is not None:
+            return col
 
-    def next_line(self):
-        if EDITOR.buffer.next_row():
-            return self.find_next_word(EDITOR.buffer.line, 0)
-
-    def find_next_blank(self, line, col):
-        if col == len(line) - 1:
-            return self.next_line()
-
-        for col, char in enumerate(line[col + 1:], start=col + 1):
-            if char == " ":
-                return self.find_next_word_from_blank(line, col + 1)
-        else:
-            return self.next_line()
-
-    def find_next_word(self, line, col):
-        if col >= len(line):
-            return self.next_line()
-
-        for col, char in enumerate(line[col + 1:], start=col + 1):
-            if char == " ":
-                return self.find_next_word_from_blank(line, col + 1)
-
-            if char in WORD_BOUNDARIES:
-                return col
-
-        else:
-            return self.next_line()
-
-    def find_next_word_from_blank(self, line, col):
-        for col, char in enumerate(line[col:], start=col):
-            if char != " ":
-                return col
-        else:
-            return self.next_line()
+        if buffer.next_row():
+            buffer.col = 0
+            return WordParser(buffer.line).next_word_start(buffer.col)
 
 
-class EndWord(InstantMovement):
-    pass
+class EndWord(WordMovements):
+    def evaluate(self, buffer):
+        col = WordParser(buffer.line).next_word_end(buffer.col)
+        if col is not None:
+            return col
+
+        if buffer.next_row():
+            buffer.col = 0
+            return WordParser(buffer.line).next_word_end(buffer.col)
 
 
-NormalMode.KEYMAP["w"] = WordMovement
+class PrevWord(WordMovements):
+    def evaluate(self, buffer):
+        col = WordParser(buffer.line).prev_word_start(buffer.col)
+        if col is not None:
+            return col
+
+        if buffer.previous_row():
+            buffer.col = len(buffer.line)
+            return WordParser(buffer.line).prev_word_start(buffer._col)  # _col because col gets limited to len(line)
+
+
+NormalMode.KEYMAP["w"] = StartWord
+NormalMode.KEYMAP["e"] = EndWord
+NormalMode.KEYMAP["b"] = PrevWord
