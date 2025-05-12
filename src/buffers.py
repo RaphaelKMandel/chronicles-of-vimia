ca39e81b7fe4526bda54e8df8a6841923ed49be3
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-05-11 20:14:53 -0400

    added text should be drawn above

--- src/buffers.py
+++ src/buffers.py
@@ -16,8 +16,6 @@ class Line:
         return self.text[item]
 
     def __init__(self, text, target):
-        self.augmented = ""
-        self.map = []
         self.words = []
         self.target = target
         self.text = text
@@ -36,37 +34,21 @@ class Line:
 
     def update(self):
         self.words = get_diff(self.text, self.target)
-        self.augmented = "".join([string for _, string in self.words])
-        self.get_map()
-
-    def get_map(self):
-        self.map = [None] * (len(self.text) + 2)
-        n1 = n2 = 0
-        for word in self.words:
-            if word[0] in {"equal", "delete"}:
-                for _ in word[1]:
-                    self.map[n1] = n2
-                    n1 += 1
-                    n2 += 1
-
-            else:
-                n2 += len(word[1])
-
-        self.map[n1] = n2
-
-    def get_coord(self, row, col):
-        return CHAR_WIDTH * col, CHAR_HEIGHT * row
-
-    def get_x_coord(self, col):
-        return CHAR_WIDTH * self.map[col]
 
     def draw(self, x, y):
         col = 0
         for op, string in self.words:
+            dy = 0
+            if op == "insert":
+                string = "^" + string
+                dy = -CHAR_HEIGHT
+
             text_surface = FONT.render(string, True, Line.COLORS[op])
-            text_rect = text_surface.get_rect(topleft=(x + CHAR_WIDTH * col, y))
+            text_rect = text_surface.get_rect(topleft=(x + CHAR_WIDTH * col, y + dy))
             SCREEN.blit(text_surface, text_rect)
-            col += len(string)
+
+            if op != "insert":
+                col += len(string)
 
 
 class Buffer:
@@ -81,13 +63,13 @@ class Buffer:
         self.score = score
 
     def get_rect(self):
-        max_width = max([len(line.augmented) for line in self.lines])
-        return self.x, self.y, max_width * CHAR_WIDTH, len(self.lines) * CHAR_HEIGHT
+        max_width = max([len(line.text) for line in self.lines])
+        return self.x, self.y, max_width * CHAR_WIDTH, 2 * CHAR_HEIGHT * len(self.lines)
 
     def get_coord(self, row, col):
         return (
-            self.y + row * CHAR_HEIGHT,
-            self.x + self.lines[row].map[col] * CHAR_WIDTH
+            self.y + (2 * row + 1) * CHAR_HEIGHT,
+            self.x + col * CHAR_WIDTH
         )
 
     @property
@@ -130,19 +112,21 @@ class Buffer:
 
         # Draw text
         for n, line in enumerate(self.lines):
-            line.draw(self.x, self.y + CHAR_HEIGHT * n)
+            line.draw(self.x, self.y + (2 * n + 1) * CHAR_HEIGHT)
 
         self.y += 0.01 * self.editor.credit
 
-        # footer = FONT.render(self.name, True, WHITE)
-        # x, y, l, h = self.get_rect()
-        # footer_rect = footer.get_rect(topleft=(x, y + h))
-        # self.editor.screen.blit(footer, footer_rect)
-        #
-        # footer = FONT.render(f"Points: {self.score}", True, WHITE)
-        # x, y, l, h = self.get_rect()
-        # footer_rect = footer.get_rect(topleft=(x + l - CHAR_WIDTH * 10, y + h))
-        # self.editor.screen.blit(footer, footer_rect)
+    def draw_buffer_name(self):
+        footer = FONT.render(self.name, True, WHITE)
+        x, y, l, h = self.get_rect()
+        footer_rect = footer.get_rect(topleft=(x, y + h))
+        self.editor.screen.blit(footer, footer_rect)
+
+    def draw_buffer_score(self):
+        footer = FONT.render(f"Points: {self.score}", True, WHITE)
+        x, y, l, h = self.get_rect()
+        footer_rect = footer.get_rect(topleft=(x + l - CHAR_WIDTH * 10, y + h))
+        self.editor.screen.blit(footer, footer_rect)
 
     def is_solved(self):
         return all([line.is_solved() for line in self.lines])
@@ -161,4 +145,4 @@ class Buffer:
 
     def delete(self):
         del self.editor.buffers[self.name]
-        self.editor.state = self.editor.normal
+        self.editor.pop()
